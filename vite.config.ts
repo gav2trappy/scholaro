36117d4fd52ba1af4910a98dcfe34abc4d0fce8c
gav2trappy <gav.biz84@gmail.com>
2025-04-24 15:08:21 +0100

    Prepare for GitHub Pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'scholaro', // <-- Add this line
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
